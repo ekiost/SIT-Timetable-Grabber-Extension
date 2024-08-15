054d0754e59c26dbc89dd34bfe87b7c33ab3069c
ekiost <choonkeatling@icloud.com>
2024-08-15 15:58:55 +0800

    chore: Add flake.nix and flake.lock files for flake dev

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    systems.url = "github:nix-systems/default";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs, systems }:
+    let
+      forEachSystem =
+        f: nixpkgs.lib.genAttrs (import systems) (system: f { pkgs = import nixpkgs { inherit system; }; });
+    in
+    {
+      devShells = forEachSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              # Add your devShell packages here
+              nodejs_22
+            ];
+          };
+        }
+      );
+    };
+}
