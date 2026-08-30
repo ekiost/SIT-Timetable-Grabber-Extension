a06fcc4dbf4431f0a80e4ba5b4ceacf46ecc18f1
ChristinaHart888 <christinahart888@gmail.com>
2026-08-30 23:06:23 +0800

    Updated dependencies, removed major vulnerabilities

--- content/content.tsx
+++ content/content.tsx
@@ -1,18 +1,19 @@
-import ReactDOM from 'react-dom/client'
-import ContentApp from './ContentApp'
-import './base.css'
-import './content.css'
+import ReactDOM from "react-dom/client";
+import ContentApp from "./ContentApp";
+import "./base.css";
+import "./content.css";
 
-setTimeout(initial, 1000)
+setTimeout(initial, 1000);
 
 function initial() {
   // Create a new div element and append it to the document's body
-  const rootDiv = document.createElement('div')
-  rootDiv.id = 'extension-root'
-  document.body.appendChild(rootDiv)
+  console.log("Initializing content script...");
+  const rootDiv = document.createElement("div");
+  rootDiv.id = "extension-root";
+  document.body.appendChild(rootDiv);
 
   // Use `createRoot` to create a root, then render the <App /> component
   // Note that `createRoot` takes the container DOM node, not the React element
-  const root = ReactDOM.createRoot(rootDiv)
-  root.render(<ContentApp />)
+  const root = ReactDOM.createRoot(rootDiv);
+  root.render(<ContentApp />);
 }
